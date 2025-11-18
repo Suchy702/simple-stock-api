d298b2aeb0b5a17308ab83382f499139c4b33de8
Suchy702 <wojtek.suchy6@wp.pl>
2025-11-18 12:10:07 +0100

    prepare for heroku

--- app.py
+++ app.py
@@ -99,4 +99,7 @@ def get_stock_info():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    # Lokalnie możesz ustawić PORT, a Heroku zrobi to automatycznie
+    import os
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
